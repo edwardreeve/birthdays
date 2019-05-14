1c35b796dce01eb8979b4e5f18cf6ccc4896e6cb
edwardreeve <edreeve+git@gmail.com>
2019-05-14 14:57:54 +0100

    Saving my progress

--- lib/birthday_list.rb
+++ lib/birthday_list.rb
@@ -1,1 +1,22 @@
 
+class BirthdayList
+  attr_reader :list
+  def initialize
+    @list = []
+  end
+
+  def add(name, date)
+    @list << { name: name, birthday: date }
+  end
+
+  def view
+    @list.each do |hash|
+      puts "#{hash[:name]}, #{hash[:birthday]}"
+    end
+  end
+end
+
+# mine = BirthdayList.new
+# mine.add('Ed', '1979-01-19')
+# mine.add('Judith', '1980-04-07')
+# mine.view
--- spec/birthday_list_spec.rb
+++ spec/birthday_list_spec.rb
@@ -1,1 +1,30 @@
+require 'birthday_list'
 
+describe BirthdayList do
+
+  let(:bdl) { BirthdayList.new }
+
+  it 'should be an instance of the BL class' do
+    expect(bdl).to be_an_instance_of(BirthdayList)
+  end
+
+  it { should respond_to(:add).with(2).arguments }
+
+  it 'should store the arguments given by add' do
+    mine = BirthdayList.new
+    mine.add('Ed', '1979-01-19')
+    mine.add('Judith', '1980-04-07')
+    expect(mine.list).to eq([{ name: 'Ed', birthday: '1979-01-19'}, 
+                        { name: 'Judith', birthday: '1980-04-07'}])
+  end
+
+  it { should respond_to :view }
+
+  it 'should return a nicely formatted list in response to view' do
+    mine = BirthdayList.new
+    mine.add('Ed', '1979-01-19')
+    mine.add('Judith', '1980-04-07')
+    # mine = [{ name: 'Ed', birthday: '1979-01-19'}, { name: 'Judith', birthday: '1980-04-07'}]
+    expect{mine.view}.to output("Ed, 1979-01-19\nJudith, 1980-04-07\n").to_stdout
+  end
+end
